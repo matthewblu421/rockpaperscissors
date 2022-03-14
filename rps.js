37a449faf57ebed6bcdeb415778fff809abcfeb2
Matt <blooom@gmail.com>
2022-03-14 10:44:08 -0400

    changed alert message

--- rps.js
+++ rps.js
@@ -2,112 +2,106 @@ let playerWins = 0;
 let computerWins = 0;
 let ties = 0;
 
-
-function computerPlay(){
-    let randomNum = (Math.floor(Math.random() * 3));
-    if (randomNum === 0) {
-        
-        return "Rock";        
-    } else if (randomNum === 1) {
-       
-        return "Paper";
-    } else if (randomNum === 2) {
-        
-        return "Scissors";
-    }
+function computerPlay() {
+  let randomNum = Math.floor(Math.random() * 3);
+  if (randomNum === 0) {
+    return "Rock";
+  } else if (randomNum === 1) {
+    return "Paper";
+  } else if (randomNum === 2) {
+    return "Scissors";
+  }
 }
 
 function playerPlay() {
-    while(true) {
+  while (true) {
     let playerChoice = prompt("Rock, Paper, or Scissors?");
     let playerChoiceCaps = playerChoice.toLowerCase();
-    
-    if(playerChoiceCaps === "rock") {        
-        return "Rock";
+
+    if (playerChoiceCaps === "rock") {
+      return "Rock";
     } else if (playerChoiceCaps === "scissors") {
-        return "Scissors";
+      return "Scissors";
     } else if (playerChoiceCaps === "paper") {
-        return "Paper";
+      return "Paper";
     }
 
-    alert("Invalid input! Please type \"Rock\", \"Paper\", or \"Scissors\"")
-}
-}
-
-function playRound(playerSelection, computerSelection)
-{
-    if (((playerSelection === "Rock") && (computerSelection === "Scissors")) ||
-    ((playerSelection === "Scissors") && (computerSelection === "Paper")) ||
-    ((playerSelection === "Paper") && (computerSelection === "Rock")))
-    {
-        playerWins++;
-        gameresults.textContent = (`${playerSelection} beats ${computerSelection}! You win the round.`);
-        game();
-        return (`${playerSelection} beats ${computerSelection}! You win the round.`);
-    }
-    else if (((playerSelection === "Scissors") && (computerSelection === "Rock")) ||
-    ((playerSelection === "Rock") && (computerSelection === "Paper")) ||
-    ((playerSelection === "Paper") && (computerSelection === "Scissors")))
-    {   
-        computerWins++;
-        gameresults.textContent = (`${computerSelection} beats ${playerSelection}! You lose the round.`);
-        game();
-    return (`${computerSelection} beats ${playerSelection}! You lose the round.`);
-
-    } else if (((playerSelection === "Scissors") && (computerSelection === "Scissors")) ||
-    ((playerSelection === "Paper") && (computerSelection === "Paper")) ||
-    ((playerSelection === "Rock") && (computerSelection === "Rock")))    
-    {   ties++;
-        gameresults.textContent = (`${playerSelection} ties ${computerSelection}: Re-play the round.`);
-        game();
-        return (`${playerSelection} matches ${computerSelection}: it's a tie!`);
-    }
-
+    alert('Invalid input! Please type "Rock", "Paper", or "Scissors"');
+  }
 }
 
-function game() {   
-    // for (let i = 0; i < 5; i++) {
-    //     playRound(playerPlay(), computerPlay());      
-    // }
-    
-    // if(playerWins > computerWins) {
-    //     console.log(`You win the game by a score of ${playerWins} to ${computerWins}`);
-    // }
-    // if(computerWins > playerWins) {
-    //     console.log(`The computer wins by a score of ${playerWins} to ${computerWins}`);
-    // }
-    // if(computerWins === playerWins) {
-    //     console.log(`It's a tie! Final score: Player: ${playerWins}. Computer: ${computerWins}`);
-    // }
-
-    if(playerWins === 5){             
-       
-        gamescore.textContent = (`Player wins the match! Final score: ${playerWins} to ${computerWins}. Congratulations! \n
-        Reload to play again.`);
-       
-        document.getElementById("button1").disabled = true;
-        document.getElementById("button2").disabled = true;
-        document.getElementById("button3").disabled = true;
-        window.alert("Maday loves Cwissy!");
-    } 
-
-    else if(computerWins === 5) {   
-        
-        document.getElementById("button1").disabled = true;
-        document.getElementById("button2").disabled = true;
-        document.getElementById("button3").disabled = true;       
-     
-        gamescore.textContent = (`Computer wins the match! Final score: ${computerWins} to ${playerWins}. Better luck next time :( \n
-            Reload to play again.`);
-            window.alert("Maday loves Cwissy!"); 
-      
-    } else {
-     
-        gamescore.textContent = (`Player: ${playerWins} points | Computer:  ${computerWins} points.`);
-    }
+function playRound(playerSelection, computerSelection) {
+  if (
+    (playerSelection === "Rock" && computerSelection === "Scissors") ||
+    (playerSelection === "Scissors" && computerSelection === "Paper") ||
+    (playerSelection === "Paper" && computerSelection === "Rock")
+  ) {
+    playerWins++;
+    gameresults.textContent = `${playerSelection} beats ${computerSelection}! You win the round.`;
+    game();
+    return `${playerSelection} beats ${computerSelection}! You win the round.`;
+  } else if (
+    (playerSelection === "Scissors" && computerSelection === "Rock") ||
+    (playerSelection === "Rock" && computerSelection === "Paper") ||
+    (playerSelection === "Paper" && computerSelection === "Scissors")
+  ) {
+    computerWins++;
+    gameresults.textContent = `${computerSelection} beats ${playerSelection}! You lose the round.`;
+    game();
+    return `${computerSelection} beats ${playerSelection}! You lose the round.`;
+  } else if (
+    (playerSelection === "Scissors" && computerSelection === "Scissors") ||
+    (playerSelection === "Paper" && computerSelection === "Paper") ||
+    (playerSelection === "Rock" && computerSelection === "Rock")
+  ) {
+    ties++;
+    gameresults.textContent = `${playerSelection} ties ${computerSelection}: Re-play the round.`;
+    game();
+    return `${playerSelection} matches ${computerSelection}: it's a tie!`;
+  }
 }
-document.getElementById("button1").addEventListener("click", () => playRound("Rock", computerPlay()));
-document.getElementById("button2").addEventListener("click", () => playRound("Paper", computerPlay()));
-document.getElementById("button3").addEventListener("click", () =>  playRound("Scissors", computerPlay()));
 
+function game() {
+  // for (let i = 0; i < 5; i++) {
+  //     playRound(playerPlay(), computerPlay());
+  // }
 
+  // if(playerWins > computerWins) {
+  //     console.log(`You win the game by a score of ${playerWins} to ${computerWins}`);
+  // }
+  // if(computerWins > playerWins) {
+  //     console.log(`The computer wins by a score of ${playerWins} to ${computerWins}`);
+  // }
+  // if(computerWins === playerWins) {
+  //     console.log(`It's a tie! Final score: Player: ${playerWins}. Computer: ${computerWins}`);
+  // }
+
+  if (playerWins === 5) {
+    gamescore.textContent = `Player wins the match! Final score: ${playerWins} to ${computerWins}. Congratulations! \n
+        Reload to play again.`;
+
+    document.getElementById("button1").disabled = true;
+    document.getElementById("button2").disabled = true;
+    document.getElementById("button3").disabled = true;
+    window.alert("You win! =)");
+  } else if (computerWins === 5) {
+    document.getElementById("button1").disabled = true;
+    document.getElementById("button2").disabled = true;
+    document.getElementById("button3").disabled = true;
+
+    gamescore.textContent = `Computer wins the match! Final score: ${computerWins} to ${playerWins}. Better luck next time :( \n
+            Reload to play again.`;
+    window.alert("Computer wins =(");
+  } else {
+    gamescore.textContent = `Player: ${playerWins} points | Computer:  ${computerWins} points.`;
+  }
+}
+document
+  .getElementById("button1")
+  .addEventListener("click", () => playRound("Rock", computerPlay()));
+document
+  .getElementById("button2")
+  .addEventListener("click", () => playRound("Paper", computerPlay()));
+document
+  .getElementById("button3")
+  .addEventListener("click", () => playRound("Scissors", computerPlay()));
